06718530b7d14006625309e460e2da4c9c4b2eca
Jakub Wodarski <JW303467@student.polsl.pl>
2022-02-08 22:08:46 +0100

    zmiana w projekcie 2

--- src/pl/zajecia/Main.java
+++ src/pl/zajecia/Main.java
@@ -3,6 +3,6 @@ package pl.zajecia;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("tu testuje gita 2");
+        System.out.println("tu testuje gita 3");
     }
 }
